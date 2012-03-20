e1ceeaa04d5a4329a5523afe2ee0a184f950c2b8
unknown <javiacosta@gmail.com>
2012-03-19 23:56:07 -0300

    Elimino AdminBundle

--- src/MyCom/AdminBundle/MyComAdminBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MyCom\AdminBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class MyComAdminBundle extends Bundle
-{
-}
--- src/MyCom/AdminBundle/Resources/views/Default/index.html.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-// Retrieves an item by its path in the main menu
-$item = $view['knp_menu']->get('AcmeDemoBundle:Builder:main');
-
-// Render an item
-echo $view['knp_menu']->render($item, array(), 'list');
-?>
